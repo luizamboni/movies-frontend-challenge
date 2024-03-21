129c42aa2b21268d89a5e27ede25adea5c49f682
Luiz Zamboni <luizamboni2002@hotmail.com>
2024-03-21 12:56:54 -0300

    all the api calls

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,141 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import api, { YearWithMultipleWinners, ProducerWinIntervals, StudiosWithWinCount, Movie, MovieResponse } from './external/moviesApi'; // Adjust the import path as needed
 
 function App() {
+  const [yearWithMultipleWinners, setYearWithMultipleWinners] = useState<YearWithMultipleWinners | null>(null);
+  const [producerWinIntervals, setProducerWinIntervals] = useState<ProducerWinIntervals | null>(null);
+  const [studiosWithWinCount, setStudiosWithWinCount] = useState<StudiosWithWinCount | null>(null);
+  const [winnersByYear, setWinnersByYear] = useState<Movie[] | null>(null);
+  const [movies, setMovies] = useState<MovieResponse | null>(null);
+
+  const year = 2018;
+
+  useEffect(() => {
+    async function fetchYearWithMultipleWinners() {
+      try {
+        const result = await api.getYearWithMultipleWinners();
+        setYearWithMultipleWinners(result);
+      } catch (error) {
+        console.error("Failed to fetch year with multiple winners:", error);
+      }
+    }
+
+    async function fetchProducerWinIntervals() {
+      try {
+        const result = await api.getProducersWithLongestAndShortestIntervalBetweenWins();
+        setProducerWinIntervals(result);
+      } catch (error) {
+        console.error("Failed to fetch producer win intervals:", error);
+      }
+    }
+
+    async function fetchStudiosWithWinCount() {
+      try {
+        const result = await api.getStudiosWithWinCount();
+        setStudiosWithWinCount(result);
+      } catch (error) {
+        console.error("Failed to fetch studios with win count:", error);
+      }
+    }
+
+    async function fetchWinnersByYear() {
+      try {
+        const result = await api.getWinnersByYear(year);
+        setWinnersByYear(result);
+      } catch (error) {
+        console.error(`Failed to fetch winners by year ${year}:`, error);
+      }
+    }
+
+    async function fetchMovies() {
+      try {
+        const moviesResponse = await api.getMovies();
+        setMovies(moviesResponse);
+      } catch (error) {
+        console.error("Failed to fetch movies:", error);
+      }
+    }
+
+    fetchYearWithMultipleWinners();
+    fetchProducerWinIntervals();
+    fetchStudiosWithWinCount();
+    fetchWinnersByYear();
+    fetchMovies();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {yearWithMultipleWinners && (
+          <div>
+            <h2>Years With Multiple Winners</h2>
+            <ul>
+              {yearWithMultipleWinners.years?.map((yearData, index) => (
+                <li key={index}>{`Year: ${yearData.year}, Winner Count: ${yearData.winnerCount}`}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+        {producerWinIntervals && (
+          <div>
+            <h2>Producers With Longest and Shortest Intervals</h2>
+            <div>
+              <h3>Shortest Intervals</h3>
+              <ul>
+                {producerWinIntervals.min?.map((interval, index) => (
+                  <li key={'min' + index}>{`${interval.producer} (${interval.interval} years between ${interval.previousWin} and ${interval.followingWin})`}</li>
+                ))}
+              </ul>
+            </div>
+            <div>
+              <h3>Longest Intervals</h3>
+              <ul>
+                {producerWinIntervals.max?.map((interval, index) => (
+                  <li key={'max' + index}>{`${interval.producer} (${interval.interval} years between ${interval.previousWin} and ${interval.followingWin})`}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        )}
+        {studiosWithWinCount && (
+          <div>
+            <h2>Studios With Win Count</h2>
+            <ul>
+              {studiosWithWinCount.studios?.map((studio, index) => (
+                <li key={index}>{`${studio.name}: ${studio.winCount} wins`}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+        {winnersByYear && (
+          <div>
+            <h2>Winners of {year}</h2>
+            
+            <ul>
+              {winnersByYear?.map((winner, index) => (
+                <li key={index}>{`Name: ${winner.title}`}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+
+        {movies ? (
+          <div>
+            <h2>Movie List</h2>
+            <ul>
+              {movies.content.map((movie, index) => (
+                <li key={index}>
+                  {`${movie.title} (${movie.year}) - Studios: ${movie.studios.join(', ')} - Producers: ${movie.producers.join(', ')}`}
+                  {movie.winner && <strong> Winner</strong>}
+                </li>
+              ))}
+            </ul>
+            <p>Total Movies: {movies.totalElements}</p>
+          </div>
+        ) : (
+          <p>Loading movies...</p>
+        )}
       </header>
     </div>
   );
---
+++ src/external/moviesApi.ts
@@ -0,0 +1,158 @@
+const baseUrl = "https://tools.texoit.com/backend-java/api/movies";
+
+interface YearWinner {
+  year: number;
+  winnerCount: number;
+}
+
+export interface YearWithMultipleWinners {
+  years: YearWinner[];
+}
+
+interface ProducerWinInterval {
+  producer: string;
+  interval: number;
+  previousWin: number;
+  followingWin: number;
+}
+
+export interface ProducerWinIntervals {
+  min: ProducerWinInterval[];
+  max: ProducerWinInterval[];
+}
+
+interface StudioWinCount {
+  name: string;
+  winCount: number; // for this key, it have to be a differente interface to YearWinner
+}
+
+export interface StudiosWithWinCount {
+  studios: StudioWinCount[];
+}
+
+export interface Movie {
+    id: number;
+    year: number;
+    title: string;
+    studios: string[];
+    producers: string[];
+    winner: boolean;
+}
+
+interface SortDetails {
+    sorted: boolean;
+    unsorted: boolean;
+}
+
+interface Pageable {
+    sort: SortDetails;
+    pageSize: number;
+    pageNumber: number;
+    offset: number;
+    paged: boolean;
+    unpaged: boolean;
+}
+  
+export interface MovieResponse {
+    content: Movie[];
+    pageable: Pageable;
+    totalElements: number;
+    last: boolean;
+    totalPages: number;
+    first: boolean;
+    sort: SortDetails;
+    number: number;
+    numberOfElements: number;
+    size: number;
+}
+  
+
+
+async function getYearWithMultipleWinners(): Promise<YearWithMultipleWinners> {
+  const url = `${baseUrl}/?projection=years-with-multiple-winners`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data: YearWithMultipleWinners = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Fetching error:", error);
+    throw error;
+  }
+}
+
+async function getProducersWithLongestAndShortestIntervalBetweenWins(): Promise<ProducerWinIntervals> {
+  const url = `${baseUrl}/?projection=max-min-win-interval-for-producers`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data: ProducerWinIntervals = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Fetching error:", error);
+    throw error;
+  }
+}
+
+async function getStudiosWithWinCount(): Promise<StudiosWithWinCount> {
+  const url = `${baseUrl}/?projection=studios-with-win-count`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data: StudiosWithWinCount = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Fetching error:", error);
+    throw error;
+  }
+}
+
+async function getWinnersByYear(year: number): Promise<Movie[]> {
+  const url = `${baseUrl}/?winner=true&year=${year}`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data: Movie[] = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Fetching error:", error);
+    throw error;
+  }
+}
+
+async function getMovies(): Promise<MovieResponse> {
+    const url = `${baseUrl}/?page=0&size=99&winner=true&year=2018`;
+  
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data: MovieResponse = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Fetching error:", error);
+      throw error;
+    }
+  }
+  
+
+export default {
+  getYearWithMultipleWinners,
+  getProducersWithLongestAndShortestIntervalBetweenWins,
+  getStudiosWithWinCount,
+  getWinnersByYear,
+  getMovies,
+};
