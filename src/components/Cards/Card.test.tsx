dd3357fde6efeb75c2074a88cfd80fe7ceb18baf
Luiz Zamboni <luizamboni2002@hotmail.com>
2024-03-23 22:22:17 -0300

    adding Card test

---
+++ src/components/Cards/Card.test.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Card from "./Card";
+
+jest.mock("../Loading/Loading", () => {
+  return function DummyLoading() {
+    return <div data-testid="loading">Loading...</div>;
+  };
+});
+
+describe("Card Component", () => {
+  test("renders the Loading component when isLoading is true", () => {
+    render(<Card isLoading={true} title="Test Title">Test Children</Card>);
+
+    const loadingElement = screen.getByTestId("loading");
+    expect(loadingElement).toBeInTheDocument();
+  });
+
+  test("does not render the Loading component when isLoading is false", () => {
+    render(<Card isLoading={false} title="Test Title">Test Children</Card>);
+
+    const loadingElement = screen.queryByTestId("loading");
+    expect(loadingElement).not.toBeInTheDocument();
+  });
+
+  test("renders the title and children correctly", () => {
+    const title = "Test Title";
+    const children = "Test Children";
+    render(<Card title={title}>{children}</Card>);
+
+    expect(screen.getByText(title)).toBeInTheDocument();
+    expect(screen.getByText(children)).toBeInTheDocument();
+  });
+});
--- src/components/Cards/Card.tsx
+++ src/components/Cards/Card.tsx
@@ -11,7 +11,7 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ title, isLoading = false, children }) => {
   return (
     <div className={style.card}>
-      <h2>{title}</h2>
+      {title && <h2>{title}</h2>}
       {children} {isLoading && <Loading/>}
     </div>
   );
